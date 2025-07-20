c602b9bf2b159e9083d0d302667cde6350afedf8
Mathieu BROUTIN <mbroutin@sedona.fr>
2025-07-20 13:49:51 +0200

    clean: remove unused code

--- src/routes/(authenticated)/admin/pools/[pool_id=pool_id]/+page.svelte
+++ src/routes/(authenticated)/admin/pools/[pool_id=pool_id]/+page.svelte
@@ -1,29 +1,9 @@
 <script lang="ts">
-  import { enhance } from '$app/forms';
 
   let { data } = $props();
 </script>
 
 <main>
-  <form action="?/state" use:enhance method="POST">
-    <button name="state" class:selected={data.state.state === 'NOT_STARTED'} value="NOT_STARTED">
-      Non démarré
-    </button>
-    <button name="state" class:selected={data.state.state === 'QUESTION'} value="QUESTION_1">
-      Question 1
-    </button>
-    <button name="state" class:selected={data.state.state === 'FINISHED'} value="FINISHED">
-      Fermer
-    </button>
-  </form>
-
   <pre>{JSON.stringify(data.pool, null, 2)}</pre>
   <pre>{JSON.stringify(data.questions, null, 2)}</pre>
-
 </main>
-
-<style lang="scss">
-  button.selected {
-    border-color: var(--color-primary-500);
-  }
-</style>
