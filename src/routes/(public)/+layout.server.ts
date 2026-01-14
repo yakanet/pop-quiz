4e2bfe15d38c55e62d2529ee9d8754b6f849bda0
Mathieu BROUTIN <mbroutin@sedona.fr>
2026-01-14 18:37:24 +0100

    refactor(public): comment out `sameSite` configuration for cookies in layout server

--- src/routes/(public)/+layout.server.ts
+++ src/routes/(public)/+layout.server.ts
@@ -25,7 +25,7 @@ async function getOrCreateAnonymousUser(): Promise<string> {
       maxAge: 60 * 60 * 24 * 365 * 10,
       httpOnly: !dev,
       secure: !dev,
-      sameSite: 'lax',
+      //sameSite: 'lax',
       path: '/',
     });
   }
