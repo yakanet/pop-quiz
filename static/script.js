4b9d124a0cfe949ebe2b94c567003d31e2d6361b
Frank <frank.broutin@gmail.com>
2026-03-22 15:55:09 +0100

    remote

--- static/script.js
+++ static/script.js
@@ -2,8 +2,8 @@
     let currentPage = parseInt(window.location.pathname.match(/P(\d+)\.html/)?.[1] || 0);
     const maxPage = 20; // Adjust this value to your max page number
     
-    const jsURL="http://192.168.1.22:5173";
-    //const jsURL="https://popquiz.pezenasentransition.org";
+    //const jsURL="http://192.168.1.22:5173";
+    const jsURL="https://popquiz.pezenasentransition.org";
 
     document.addEventListener('keydown', (event) => {
         if (event.key === 'ArrowRight' && currentPage < maxPage) {
